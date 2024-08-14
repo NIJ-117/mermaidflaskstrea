6150bbe21889f3038e93385f7b64d3a67f3b22d5
NIJ-117 <weareborngeniusalways@gmail.com>
2024-08-14 05:54:29 +0000

    dfdee

--- crewflask/app.py
+++ crewflask/app.py
@@ -96,4 +96,4 @@ def update_diagram():
     return jsonify({'updatedMarkdown': updated_markdown})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=4000)
+    app.run(debug=True, host="0.0.0.0", port=4001)
